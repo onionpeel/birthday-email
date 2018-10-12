ea6e6e46586b4ee3ebf14ddf9a8d5ac4583c4988
Jeff Dinkelman <mfankenstein@yahoo.com>
2018-10-12 16:08:21 -0700

    set cron time

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const {findBirthdays} = require('./findandSendBirthdays/findBirthdays');
 const {dateToday} = require('./../utilities/dateToday');
 
-let task = cron.schedule('57 14 * * *', () => {
+let task = cron.schedule('11 16 * * *', () => {
   console.log('cron test succeeded!')
   let today = dateToday();
   findBirthdays(today);
